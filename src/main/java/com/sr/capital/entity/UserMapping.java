81243d34966c7d51cfce5b5810ab93a436537c3c
Vikas Sinwar <vikas.kumar@pickrr.com>
2024-06-06 12:02:00 +0530

    add user mapping entity

---
+++ src/main/java/com/sr/capital/entity/UserMapping.java
@@ -0,0 +1,30 @@
+package com.sr.capital.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static com.sr.capital.helpers.constants.Constants.EntityNames.USER;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = USER)
+@Inheritance(
+        strategy = InheritanceType.JOINED
+)
+public class UserMapping extends LongBaseEntity{
+
+
+    @Column(name = "capital_user_id")
+    private Long capitalUserId;
+
+    @Column(name = "sr_user_id")
+    private Long srUserId;
+
+    @Column(name = "sr_company_id")
+    private Long srCompanyId;
+
+}
---
+++ src/main/java/com/sr/capital/helpers/enums/DirectorDesignation.java
@@ -0,0 +1,12 @@
+package com.sr.capital.helpers.enums;
+
+public enum DirectorDesignation {
+
+    DIRECTOR("DIRECTOR");
+
+    String type;
+
+    DirectorDesignation(String type){
+        this.type =type;
+    }
+}
