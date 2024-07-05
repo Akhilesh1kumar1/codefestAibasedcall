908835c841f0ad6d4e7577fb9852906c91964348
Vikas Sinwar <vikas.kumar@pickrr.com>
2024-07-05 10:47:03 +0530

    add file support

--- src/main/java/com/sr/capital/helpers/constants/DocExtractionConstants.java
+++ src/main/java/com/sr/capital/helpers/constants/DocExtractionConstants.java
@@ -10,8 +10,14 @@ public class DocExtractionConstants {
         public static final String JPG = "image/jpg";
         public static final String JPEG = "image/jpeg";
         public static final String PNG = "image/png";
+        public static final String CSV = "text/csv";
 
-        public static final List<String> supportedFileTypes = List.of(PDF, JPG, JPEG, PNG);
+        public static final String EXCEL = "application/vnd.ms-excel";
+
+        public static final String EXCELS = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+
+        public static final List<String> supportedFileTypes = List.of(PDF, JPG, JPEG, PNG,CSV,EXCEL, EXCELS);
     }
 
     public static final Integer LIST_SIZE = 10000;
