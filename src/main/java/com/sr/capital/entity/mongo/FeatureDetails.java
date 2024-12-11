b041e29b60e99a109a84fb73f465111f8e387061
rishabhsharma <rishabh.sharma@shiprocket.com>
2024-12-11 17:31:19 +0530

    Changes in sava and update

--- src/main/java/com/sr/capital/entity/mongo/FeatureDetails.java
+++ src/main/java/com/sr/capital/entity/mongo/FeatureDetails.java
@@ -1,18 +1,18 @@
 package com.sr.capital.entity.mongo;
 
 import com.sr.capital.entity.mongo.kyc.BaseDoc;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
-@Builder
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Document("feature_details")
+@Builder
 public class FeatureDetails extends BaseDoc {
 
     @Indexed(unique = false,name = "srCompanyId")
--- src/main/java/com/sr/capital/service/impl/ConfigServiceImpl.java
+++ src/main/java/com/sr/capital/service/impl/ConfigServiceImpl.java
@@ -6,6 +6,7 @@ import com.sr.capital.dto.response.FeatureDetailResponseDto;
 import com.sr.capital.entity.mongo.FeatureDetails;
 import com.sr.capital.repository.mongo.FeatureDetailRepository;
 import com.sr.capital.service.ConfigService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -33,11 +34,16 @@ public class ConfigServiceImpl implements ConfigService {
     public FeatureDetailResponseDto saveCompanyWithFeature(FeatureDetailRequestDto featureDetailRequestDto){
         FeatureDetails featureDetails = featureDetailRepository.findBySrCompanyId(Long.parseLong(RequestData.getTenantId()));
         List<String> cleanedFeatures = featureDetailRequestDto.getFeature().stream()
-                .filter(feature -> feature != null && !feature.isEmpty())
+                .filter(feature -> feature != null && !feature.isBlank())
                 .distinct()
-                .collect(Collectors.toList());
+                .toList();
+        if(featureDetails == null){
+            featureDetails = new FeatureDetails();
+        }
         featureDetails.setFeature(cleanedFeatures);
-        featureDetailRepository.save(featureDetails);
+        featureDetails.setSrCompanyId(Long.parseLong(RequestData.getTenantId()));
+        featureDetails = featureDetailRepository.save(featureDetails);
+
         return FeatureDetailResponseDto.builder()
                 .srCompanyId(featureDetails.getSrCompanyId())
                 .feature(featureDetails.getFeature())
@@ -47,23 +53,28 @@ public class ConfigServiceImpl implements ConfigService {
     @Override
     public FeatureDetailResponseDto updateCompanyWithFeature(FeatureDetailRequestDto featureDetailRequestDto){
         FeatureDetails featureDetails = featureDetailRepository.findBySrCompanyId(Long.parseLong(RequestData.getTenantId()));
+        if (featureDetails != null) {
+            featureDetails = new FeatureDetails();
+            featureDetails.setSrCompanyId(Long.parseLong(RequestData.getTenantId()));
+            List<String> existingFeatures = Optional.ofNullable(featureDetails.getFeature()).orElseGet(ArrayList::new);
+            Set<String> existingFeatureSet = new HashSet<>(existingFeatures);
 
-        List<String> existingFeatures = Optional.ofNullable(featureDetails.getFeature()).orElseGet(ArrayList::new);
-        Set<String> existingFeatureSet = new HashSet<>(existingFeatures);
+            List<String> newFeatures = Optional.ofNullable(featureDetailRequestDto.getFeature()).orElseGet(Collections::emptyList);
 
-        List<String> newFeatures = Optional.ofNullable(featureDetailRequestDto.getFeature()).orElseGet(Collections::emptyList);
+            newFeatures.stream()
+                    .filter(feature -> feature != null && !feature.isBlank() && existingFeatureSet.add(feature))
+                    .forEach(existingFeatures::add);
 
-        newFeatures.stream()
-                .filter(feature -> feature != null && !feature.isBlank() && existingFeatureSet.add(feature))
-                .forEach(existingFeatures::add);
+            featureDetails.setFeature(existingFeatures);
+            featureDetails = featureDetailRepository.save(featureDetails);
 
-        featureDetails.setFeature(existingFeatures);
-        featureDetails = featureDetailRepository.save(featureDetails);
-
-        return FeatureDetailResponseDto.builder()
-                .srCompanyId(featureDetails.getSrCompanyId())
-                .feature(featureDetails.getFeature())
-                .build();
+            return FeatureDetailResponseDto.builder()
+                    .srCompanyId(featureDetails.getSrCompanyId())
+                    .feature(featureDetails.getFeature())
+                    .build();
+        } else {
+            throw new EntityNotFoundException("Feature details not found for the company ID: " + RequestData.getTenantId());
+        }
     }
 
 }
