17d978aa8560cb78e9ebd17323892459b2bf0702
Akhilesh kumar <akhilesh.kumar@shiprocket.com>
2025-01-28 12:18:49 +0530

    Update banner based on features

--- src/main/java/com/sr/capital/banner/service/FeatureBannerServiceImpl.java
+++ src/main/java/com/sr/capital/banner/service/FeatureBannerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.sr.capital.banner.service;
 
+import com.sr.capital.banner.enums.BannerResponse;
 import com.sr.capital.banner.enums.FeatureBannerEnum;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -11,10 +13,17 @@ public class FeatureBannerServiceImpl implements FeatureBannerService {
 
     @Override
     public Map<String, Object> getFeatureBannerData() {
+        List<BannerResponse> dtoList = FeatureBannerEnum.CRIF.toRisponseDtoList();
+        return Map.of(FeatureBannerEnum.CRIF.name(), dtoList);
+    }
+
+    @Override
+    public Map<String, Object> getPublicBannerData() {
         Map<String, Object> response = new HashMap<>();
 
         for (FeatureBannerEnum feature : FeatureBannerEnum.values()) {
-            response.put(feature.name(), feature.getBannerUrls());
+            Map<String, String> bannerUrls = feature.getBannerUrls();
+            response.put(feature.name(), bannerUrls.keySet());
         }
 
         return response;
